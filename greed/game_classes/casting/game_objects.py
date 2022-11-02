3337c0f135cadfd20fab90326407eca786ee2fc4
gregclar2019 <daddygreggy09@gmail.com>
2022-11-02 02:57:13 -0400

    game objects skeleton push

--- greed/game_classes/casting/game_objects.py
+++ greed/game_classes/casting/game_objects.py
@@ -1,11 +1,115 @@
 #import needed classes
+#equivalent to Actor() class in RFK
 from game_classes.shared.postion_point import PositionPoint
 from game_classes.shared.color import Color
 
-class Game_object:
+class GameObject:
     ''' A visible, moveable thing that participates in the game.
     
     Responsiblity: to keep track of it's appearance, postion and velocity in 2D space.
     
     Attributes:
         _text (string)'''
+
+    def __init__(self):
+        """Constructs a new Game Object."""
+        self._text = ""
+        self._font_size = 15
+        self._color = Color(255, 255, 255)
+        self._position = PositionPoint(0, 0)
+        self._velocity = PositionPoint(0, 0)
+
+    def get_color(self):
+        """Gets the Game Object's color as a tuple of three ints (r, g, b).
+        
+        Returns:
+            Color: The Game Object's text color.
+        """
+        return self._color
+
+    def get_font_size(self):
+        """Gets the Game Object's font size.
+        
+        Returns:
+            Point: The Game Object's font size.
+        """
+        return self._font_size
+
+    def get_position(self):
+        """Gets the Game Object's position in 2d space.
+        
+        Returns:
+            Point: The Game Object's position in 2d space.
+        """
+        return self._position
+
+    def get_text(self):
+        """Gets the Game Object's textual representation.
+        
+        Returns:
+            string: The Game Object's textual representation.
+        """
+        return self._text
+
+    def get_velocity(self):
+        """Gets the Game Object's speed and direction.
+        
+        Returns:
+            Point: The Game Object's speed and direction.
+        """
+        return self._velocity
+
+    def auto_move_down():
+        pass
+
+    def move_next(self, max_x, max_y):
+        """Moves the Game Object to its next position according to its velocity. 
+
+        
+        Args:
+            max_x (int): The maximum x value.
+            max_y (int): The maximum y value.
+        """
+        x = (self._position.get_x() + self._velocity.get_x()) % max_x
+        y = (self._position.get_y() + self._velocity.get_y()) % max_y
+        self._position = PositionPoint(x, y)
+
+    def set_color(self, color):
+        """Updates the color to the given one.
+        
+        Args:
+            color (Color): The given color.
+        """
+        self._color = color
+
+    def set_position(self, position):
+            """Updates the position to the given one.
+            
+            Args:
+                position (Point): The given position.
+            """
+            self._position = position
+    
+    def set_font_size(self, font_size):
+        """Updates the font size to the given one.
+        
+        Args:
+            font_size (int): The given font size.
+        """
+        self._font_size = font_size
+    
+    def set_text(self, text):
+        """Updates the text to the given value.
+        
+        Args:
+            text (string): The given value.
+        """
+        self._text = text
+
+    def set_velocity(self, velocity):
+        """Updates the velocity to the given one.
+        
+        Args:
+            velocity (Point): The given velocity.
+        """
+        self._velocity = velocity
