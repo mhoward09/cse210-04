b36892a8ab905e5fb853b6ff5f1ae5f108066201
sumapri <114256701+sumapri@users.noreply.github.com>
2022-11-02 21:29:57 -0500

    Update keyboard_service.py
    I hope I didn't meesed up with this class, It was supposed that I created this one and the windows class.

--- greed/game_classes/services/keyboard_service.py
+++ greed/game_classes/services/keyboard_service.py
@@ -4,19 +4,19 @@ import pyray as pr
 #import needed classes
 from game_classes.shared.postion_point import PositionPoint
 
-class KeyboardService:
+class Keyboard:
     ''' Detects player keyboard input
 
     Responsibility: detect player key presses and translate them into a PositionPoint representing a direction (left or right).
 
     Attributes:
-        cell_size (int): for scaling directional input to a grid
+        square_size (int): for scaling directional input to a grid
     '''
-    def __init__(self, cell_size):
-        ''' Constructs a new Keyboard Service using the specified cell size.
+    def __init__(self, square_size = 1):
+        ''' Constructs a new Keyboard using the specified square size.
 
         Args:
-            cell_size (int): the size of a cell in the display grid.
+            square_size (int): the size of a square in the display grid.
         '''
         #declare self variables and attributes from arguments
         pass
